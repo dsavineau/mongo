83d097b0c3029123e3b8030b31c8df59f705f5a7
Eliot Horowitz <eliot@10gen.com>
2012-12-21 11:12:56 -0500

    nsdetails takes a StringData, don't call c_str on std::string
    Conflicts:
    	src/mongo/db/btree.cpp
    	src/mongo/db/cloner.cpp
    	src/mongo/db/commands/index_stats.cpp
    	src/mongo/db/commands/storage_details.cpp
    	src/mongo/db/commands/touch.cpp
    	src/mongo/db/compact.cpp
    	src/mongo/db/dbcommands.cpp
    	src/mongo/db/dbhelpers.cpp
    	src/mongo/db/geo/2d.cpp
    	src/mongo/db/geo/geonear.cpp
    	src/mongo/db/geo/haystack.cpp
    	src/mongo/db/geo/s2cursor.cpp
    	src/mongo/db/geo/s2nearcursor.cpp
    	src/mongo/db/hashindex.cpp
    	src/mongo/db/index.cpp
    	src/mongo/db/index_update.cpp
    	src/mongo/db/namespace_details.cpp
    	src/mongo/db/pdfile.cpp
    	src/mongo/db/query_optimizer_internal.cpp
    	src/mongo/db/ttl.cpp
    	src/mongo/dbtests/btreetests.inl
    	src/mongo/s/d_migrate.cpp
    	src/mongo/s/d_split.cpp
    	src/mongo/tools/dump.cpp
    	src/mongo/util/touch_pages.cpp

--- src/mongo/db/namespace_details.cpp
+++ src/mongo/db/namespace_details.cpp
@@ -1777,7 +1777,7 @@ namespace mongo {
         BSONObj newSpec;
         {
             BSONObj oldSpec;
-            NamespaceDetails *d = nsdetails( sysNamespaces.c_str() );
+            NamespaceDetails *d = nsdetails(sysNamespaces);
             verify( d != NULL && d->findOne( BSON( "name" << from ), oldSpec ) );
             BSONObjBuilder b;
             BSONObjIterator i( oldSpec.getObjectField( "options" ) );
